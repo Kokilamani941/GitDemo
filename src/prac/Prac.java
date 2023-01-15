38a65ec382c710c6f4e9fe80b8ce266bb16d14f7
karth <kokilamani941@gmail.com>
2023-01-15 14:03:43 -0600

    First Commit!

---
+++ src/prac/Prac.java
@@ -0,0 +1,30 @@
+package prac;
+
+public class Prac{
+	
+	public static void main(String[] args) {
+	
+	
+	int x=4;
+	
+	int y=5;
+	
+	if(x==y)
+	{
+		System.out.println("value is true");
+	}
+	else if(x==y)
+	{
+		
+		System.out.println("value is false");
+
+	}
+	
+	else
+	{
+		System.out.println("value maybe false");
+
+	}
+	}
+	
+	}
